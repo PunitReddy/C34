a7ed1aa6bece65b3401e9d7c28f6d5b6b11418c6
PunitReddy <70163405+PunitReddy@users.noreply.github.com>
2020-10-06 11:59:01 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,49 @@
+var ball, database;
+
+function setup(){
+    createCanvas(500,500);
+
+    database = firebase.database()
+    var childnode = database.ref("ball/position")
+    childnode.on("value", readDatabase)
+
+    ball = createSprite(250,250,10,10);
+    ball.shapeColor = "red";
+}
+
+function draw(){
+    background("white");
+    if(keyDown(LEFT_ARROW)){
+        changePosition(-1,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+        changePosition(1,0);
+    }
+    else if(keyDown(UP_ARROW)){
+        changePosition(0,-1);
+    }
+    else if(keyDown(DOWN_ARROW)){
+        changePosition(0,+1);
+    }
+    drawSprites();
+}
+
+function changePosition(x,y){
+
+    database.ref("ball/position").set({
+
+        x: position.x + x,
+        y: position.y + y
+    })
+
+    ball.x = ball.x + x;
+    ball.y = ball.y + y;
+}
+
+function readDatabase(data){
+
+    position = data.val()
+
+    ball.x = position.x
+    ball.y = position.y
+}
